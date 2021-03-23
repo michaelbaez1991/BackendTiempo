c3506756d9a6d47a6048161e251745913c12711d
Lenovo 330s <michael.baez.mendoza1991@gmail.com>
2021-03-22 21:37:40 -0500

    add controller for CRUD

--- src/Controller/Api/NewsApiController.php
+++ src/Controller/Api/NewsApiController.php
@@ -14,4 +14,97 @@ use FOS\RestBundle\Controller\AbstractFOSRestController;
 
 class NewsApiController extends AbstractFOSRestController {
 
+    /**
+     * @Rest\Get(path="/news")
+     * @Rest\View(serializerGroups={"news"}, serializerEnableMaxDepthChecks=true)
+     */
+
+    public function getAction(NewsRepository $newsRepository) {
+        return $newsRepository->findAll();
+    }
+
+
+    /**
+     * @Rest\Post(path="/news/create")
+     * @Rest\View(serializerGroups={"news"}, serializerEnableMaxDepthChecks=true)
+     */
+    public function PostAction(Request $request, EntityManagerInterface $em){
+
+        $newsDto = new NewsDto();
+        $form = $this->createForm(NewFormType::class, $newsDto);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $new = new News();
+            $new->setTitle($newsDto->title);
+            $new->setContent($newsDto->content);
+            $em->persist($new);
+            $em->flush();
+            return $new;
+        }
+        return $form;
+    }
+    /**
+    * @Rest\Post(path="/news/{id}")
+    * @Rest\View(serializerGroups={"news"}, serializerEnableMaxDepthChecks=true)
+    */
+    public function editAction(int $id, NewsRepository $newsRepository, Request $request, EntityManagerInterface $em ) {
+
+        $new = $newsRepository->find($id);
+
+        if (!$new) {
+            throw $this->createNotFoundException("No existe");            
+        }
+
+        $newsDto = NewsDto::createFromNew($new);
+
+        $form = $this->createForm(NewFormType::class, $newsDto);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $new->setTitle($newsDto->title);
+            $new->setContent($newsDto->content);
+                
+            $em->persist($new);
+            $em->flush();
+            return $new;
+        } 
+    
+        return $form;
+    }
+
+    /**
+     * @Rest\Get(path="/news/{id}")
+     * @Rest\View(serializerGroups={"book"}, serializerEnableMaxDepthChecks=true)
+     *//*
+    public function getSingleAction(string $id, GetNew $getNew)
+    {
+        try {
+            $book = ($getNew)($id);
+        } catch (Exception $exception) {
+            return View::create('Book not found', Response::HTTP_BAD_REQUEST);
+        }
+        return $book;
+    }
+*/
+    
+
+
+    /**
+     * @Rest\Delete(path="/news/{id}")
+     * @Rest\View(serializerGroups={"news"}, serializerEnableMaxDepthChecks=true)
+     */
+    public function deleteAction(string $id, NewsRepository $newsRepository, EntityManagerInterface $em){
+
+        $new = $newsRepository->find($id);
+        if (!$new) {
+        return View::create('Noticia no encontrada', Response::HTTP_BAD_REQUEST);
+        }
+
+        $em->remove($new);
+        $em->flush();
+
+        return 'Noticia Eliminada';
+    }
+
 }
--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -12,4 +12,58 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class NewsController extends AbstractController { 
+
+    /**
+     * 
+     * @Route("news", name="news")
+     * 
+     * 
+     **/
+    
+    public function index(Request $request, NewsRepository $newRepository) {
+
+        $news = $newRepository->findAll();
+
+        $newsAsArray = [];
+
+        foreach ($news as $new ) {
+            $newsAsArray[] = [
+                'id' => $new->getId(),
+                'title' => $new->getTitle(),
+                'content' => $new->getContent(),
+                'image' => $new->getImage()
+            ];
+        }
+
+        $response = new JsonResponse();
+
+        $response->setData([
+            'success' => true,
+            'data'    => $newsAsArray 
+        ]);
+        
+        return $response;
+    }
+
+    /**
+     * 
+     * @Route("api/news/create", name="news_create")
+     * 
+     **/
+
+/* public function CreateNews(Request $request, EntityManagerInterface $en){
+        $new = new News();
+        $response = new JsonResponse();
+
+
+        $new->setTitle('Noticia Creada 1');
+        $new->setContent('Noticia Creada por diego rojas prueba 1');
+
+        $en->persist($new);
+        $en->flush();
+
+    
+
+        return $response;
+    }*/
 }
