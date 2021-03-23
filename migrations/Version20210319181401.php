c44585e951eb87d5262f36195eb8c6d0efd830ec
Lenovo 330s <michael.baez.mendoza1991@gmail.com>
2021-03-22 20:56:29 -0500

    add migrations

--- migrations/Version20210319181401.php
+++ migrations/Version20210319181401.php
@@ -12,18 +12,20 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20210319181401 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE news (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, image VARCHAR(512) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE news');
     }
 }
